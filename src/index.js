8b103321335bf85310ce93d6787a2d27f7d6426b
Shravika2004 <shravikahamjade@gmail.com>
2024-09-05 03:05:16 -0700

    ShravikaHamjade-FEcommit

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 //TODO 1 enter your key from geocode here
-export const GEOCODE_API_KEY="";
+export const GEOCODE_API_KEY="66c5f22a71e8b059470230jqkf02bf6";
 
 //TODO 2 enter your function app url here
-export const FUNCTION_APP_URL="http://localhost:7071/api/getHospitalsList";
+export const FUNCTION_APP_URL="https://fa-shravika2004.azurewebsites.net/api/getHospitalsList?code=+";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
